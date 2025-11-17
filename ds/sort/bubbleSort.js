7e285ee0e5e123dd97788cdddd06756804f9651e
Naved <naved1504@gmail.com>
2025-11-17 11:13:00 +0530

    Added Bubble Sort implementation

---
+++ ds/sort/bubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    let swapped = false;
+    for (let j = 0; j < n-i-1; j++) {
+      if (arr[j] > arr[j+1]) { [arr[j], arr[j+1]] = [arr[j+1], arr[j]]; swapped = true; }
+    }
+    if (!swapped) break;
+  }
+  return arr;
+}
+console.log(bubbleSort([5, 2, 8, 1, 9]));
+
+const _v = 9;
