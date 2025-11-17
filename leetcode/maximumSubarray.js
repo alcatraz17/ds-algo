35a0bc92d9003436789fdc95de269950dc2e7d10
Naved <naved1504@gmail.com>
2025-11-17 09:59:00 +0530

    Solved Maximum Subarray

---
+++ leetcode/maximumSubarray.js
@@ -0,0 +1,11 @@
+function maxSubArray(nums) {
+  let maxSum = nums[0], curSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    curSum = Math.max(nums[i], curSum + nums[i]);
+    maxSum = Math.max(maxSum, curSum);
+  }
+  return maxSum;
+}
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+
+const _v = 7;
