2a65f68c427d27a082b234e8f8d43f14e5c9d904
Naved <naved1504@gmail.com>
2025-11-20 11:17:00 +0530

    Solved Invert Binary Tree

---
+++ leetcode/invertBinaryTree.js
@@ -0,0 +1,8 @@
+function invertTree(root) {
+  if (!root) return null;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+}
+console.log("Invert Binary Tree solved");
+
+const _v = 19;
