4ca293fe18a45ef066b6158b2d25f201b6d4a15a
Naved <naved1504@gmail.com>
2025-11-20 10:04:00 +0530

    Solved Linked List Cycle detection

---
+++ leetcode/linkedListCycle.js
@@ -0,0 +1,12 @@
+function hasCycle(head) {
+  let slow = head, fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
+}
+console.log("Linked List Cycle detection solved");
+
+const _v = 18;
