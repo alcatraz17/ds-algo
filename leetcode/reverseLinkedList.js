4806b13e80319b5987910dba420cde5291b969e7
Naved <naved1504@gmail.com>
2025-11-22 10:53:00 +0530

    Solved Reverse Linked List

---
+++ leetcode/reverseLinkedList.js
@@ -0,0 +1,13 @@
+function reverseList(head) {
+  let prev = null, curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
+console.log("Reverse Linked List solved");
+
+const _v = 23;
