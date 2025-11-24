06288caaf6616d2beaeb218cae7bd96ef51e5131
Naved <naved1504@gmail.com>
2025-11-24 09:10:00 +0530

    Solved Symmetric Tree

---
+++ leetcode/symmetricTree.js
@@ -0,0 +1,11 @@
+function isSymmetric(root) {
+  function isMirror(t1, t2) {
+    if (!t1 && !t2) return true;
+    if (!t1 || !t2) return false;
+    return t1.val === t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+  }
+  return isMirror(root, root);
+}
+console.log("Symmetric Tree solved");
+
+const _v = 24;
