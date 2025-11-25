dd9975639b73068bf7910084a1299b7137a29514
Naved <naved1504@gmail.com>
2025-11-25 12:19:00 +0530

    Solved Subsets

---
+++ leetcode/subsets.js
@@ -0,0 +1,11 @@
+function subsets(nums) {
+  const result = [[]];
+  for (const num of nums) {
+    const len = result.length;
+    for (let i = 0; i < len; i++) result.push([...result[i], num]);
+  }
+  return result;
+}
+console.log(subsets([1,2,3]));
+
+const _v = 29;
