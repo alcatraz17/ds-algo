48d1523da120cafe382736a45c4bcfb852ca352a
Naved <naved1504@gmail.com>
2025-11-26 11:24:00 +0530

    Solved Minimum Path Sum

---
+++ leetcode/minimumPathSum.js
@@ -0,0 +1,12 @@
+function minPathSum(grid) {
+  const m = grid.length, n = grid[0].length;
+  for (let i = 1; i < m; i++) grid[i][0] += grid[i-1][0];
+  for (let j = 1; j < n; j++) grid[0][j] += grid[0][j-1];
+  for (let i = 1; i < m; i++)
+    for (let j = 1; j < n; j++)
+      grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+  return grid[m-1][n-1];
+}
+console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]));
+
+const _v = 33;
