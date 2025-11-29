7f1606c88acf40cfc60bcb6555a09f9ec441348b
Naved <naved1504@gmail.com>
2025-11-29 09:48:00 +0530

    Solved First Missing Positive

---
+++ leetcode/firstMissingPositive.js
@@ -0,0 +1,12 @@
+function firstMissingPositive(nums) {
+  const n = nums.length;
+  for (let i = 0; i < n; i++)
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] !== nums[i])
+      [nums[nums[i]-1], nums[i]] = [nums[i], nums[nums[i]-1]];
+  for (let i = 0; i < n; i++)
+    if (nums[i] !== i + 1) return i + 1;
+  return n + 1;
+}
+console.log(firstMissingPositive([3,4,-1,1]));
+
+const _v = 43;
