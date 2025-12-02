bc1ee452513e0dcc0267f7cbf084958d90d7230c
Naved <naved1504@gmail.com>
2025-12-02 09:50:00 +0530

    Solved Top K Frequent Elements

---
+++ leetcode/topKFrequentElements.js
@@ -0,0 +1,8 @@
+function topKFrequent(nums, k) {
+  const freq = new Map();
+  for (const n of nums) freq.set(n, (freq.get(n) || 0) + 1);
+  return [...freq.entries()].sort((a, b) => b[1] - a[1]).slice(0, k).map(e => e[0]);
+}
+console.log(topKFrequent([1,1,1,2,2,3], 2));
+
+const _v = 49;
