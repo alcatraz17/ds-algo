7e012dc99fe179c6560dd7eb85e151b57d3dc5b4
Naved <naved@aadrikaenterprises.com>
2026-04-01 17:09:30 +0530

    fix (prime number) Fixed prime number check logic

--- gfg/mathematics/7.primeNumber.js
+++ gfg/mathematics/7.primeNumber.js
@@ -11,8 +11,12 @@ const isPrime = (num) => {
     return false;
   }
 
-  for (let i = 5; i * i < num; i += 6) {
+  for (let i = 5; i * i <= num; i += 6) {
     if (num % i === 0) {
+      return false;
+    }
+
+    if (num % (i + 2) === 0) {
       return false;
     }
   }
