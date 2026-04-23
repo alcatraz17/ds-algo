abbafedd389ddfc8919a9efe73684e6d406257ad
Naved <naved1504@gmail.com>
2026-04-23 18:21:04 +0530

    Trying solution

---
+++ gfg/mathematics/problems/10.moduloInverse.js
@@ -0,0 +1,13 @@
+class M {
+  moduloInverse(n, m) {
+    const n = ((n % m) + m) % m;
+
+    let [old_r, r] = [n, m];
+    let [old_s, s] = [1, 0];
+
+    while (r !== 0) {}
+  }
+}
+
+const m = new M();
+const modInv = console.log(m.moduloInverse(10, 17));
