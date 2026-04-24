4b2d5edf1588ed3005be74897d60e8b6e69df997
Naved <naved1504@gmail.com>
2026-04-24 18:24:19 +0530

    Added modulo inverse solution

--- gfg/mathematics/problems/10.moduloInverse.js
+++ gfg/mathematics/problems/10.moduloInverse.js
@@ -1,13 +1,20 @@
-class M {
-  moduloInverse(n, m) {
-    const n = ((n % m) + m) % m;
+class Solution {
+  modInverse(n, m) {
+    // code here
+    let a = n,
+      b = m;
+    let x0 = 1,
+      x1 = 0;
+    while (b > 0) {
+      let q = Math.floor(a / b);
+      [a, b] = [b, a % b];
+      [x0, x1] = [x1, x0 - q * x1];
+    }
 
-    let [old_r, r] = [n, m];
-    let [old_s, s] = [1, 0];
-
-    while (r !== 0) {}
+    if (a !== 1) return -1;
+    return ((x0 % m) + m) % m;
   }
 }
 
-const m = new M();
-const modInv = console.log(m.moduloInverse(10, 17));
+const m = new Solution();
+const modInv = console.log(m.modInverse(10, 17));
