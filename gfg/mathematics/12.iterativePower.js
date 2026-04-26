fb674223e4f6e731f8c486820009851855898e91
Naved Anjum <naved1504@gmail.com>
2026-04-26 17:31:43 +0530

    Removed duplicate files

--- gfg/mathematics/12.iterativePower.js
+++
@@ -1,3 +0,0 @@
-const iterativePower = (x, y) => {
-  
-}
--- gfg/mathematics/5.greatestCommonDivisor.js
+++
@@ -1,9 +0,0 @@
-const greatestCommonDivisor = (a, b) => {
-  if (b === 0) {
-    return a;
-  }
-
-  return greatestCommonDivisor(b, a % b);
-};
-
-console.log(greatestCommonDivisor(6, 87));
