bab190850ff3e2fbe7572dd528bb6ee8c55d17b4
Naved Anjum <naved1504@gmail.com>
2026-08-19 20:30:00 +0530

    Add question statement and move explanation to end for insert at end

--- gfg/04.arrays/problems/02.insertAtEnd.js
+++ gfg/04.arrays/problems/02.insertAtEnd.js
@@ -1,4 +1,24 @@
 /*
+ * Question: Array Insert at End
+ * Given an array arr of size n and an element ele, insert ele at the end of the array and return the modified array.
+ *
+ * Examples:
+ * Input: arr[] = [1, 2, 3], ele = 5
+ * Output: [1, 2, 3, 5]
+ *
+ * Constraints:
+ * 1 ≤ n ≤ 10^5
+ * 1 ≤ arr[i], ele ≤ 10^5
+ */
+function insertAtEnd(arr, ele) {
+  arr[arr.length] = ele;
+
+  return arr;
+}
+
+console.log(insertAtEnd([1, 2, 3], 5));
+
+/*
  * Explanation
  * -----------
  * Approach: Use the fact that writing at arr[arr.length] (one past the last index) automatically grows the array by one element.
@@ -10,10 +30,3 @@
  * Time Complexity: O(1)
  * Auxiliary Space: O(1)
  */
-function insertAtEnd(arr, ele) {
-  arr[arr.length] = ele;
-
-  return arr;
-}
-
-console.log(insertAtEnd([1, 2, 3], 5));
